0b321dec87e98baa20b76e2c8ce5206ba8002886
rahunak <euhene@linnovate.net>
2024-02-08 23:53:43 +0300

    #1: refactored

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -4,59 +4,31 @@ import { simpleCalculator, Action } from './index';
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
     let result = simpleCalculator({ a: 1, b: -1, action: Action.Add });
-    expect(result).toBeGreaterThan(-0.1);
-    expect(result).toBeGreaterThanOrEqual(0);
-    expect(result).toBeLessThan(0.1);
-    expect(result).toBeLessThanOrEqual(0);
     expect(result).toBe(0);
 
     result = simpleCalculator({ a: 0.2, b: 0.1, action: Action.Add });
-    expect(result).toBeGreaterThan(0.299);
-    expect(result).toBeGreaterThanOrEqual(0.299);
-    expect(result).toBeLessThan(0.301);
-    expect(result).toBeLessThanOrEqual(0.31);
     expect(result).toBeCloseTo(0.3);
   });
 
   test('should subtract two numbers', () => {
     let result = simpleCalculator({ a: 1, b: -1, action: Action.Subtract });
-    expect(result).toBeGreaterThan(1.99);
-    expect(result).toBeGreaterThanOrEqual(1.99);
-    expect(result).toBeLessThan(2.01);
-    expect(result).toBeLessThanOrEqual(2.01);
     expect(result).toBe(2);
 
     result = simpleCalculator({ a: 1, b: 0, action: Action.Subtract });
-    expect(result).toBeGreaterThan(0.99);
-    expect(result).toBeGreaterThanOrEqual(0.99);
-    expect(result).toBeLessThan(1.01);
-    expect(result).toBeLessThanOrEqual(1.01);
     expect(result).toBe(1);
   });
 
   test('should multiply two numbers', () => {
     let result = simpleCalculator({ a: 1, b: -1, action: Action.Multiply });
-    expect(result).toBeGreaterThan(-1.01);
-    expect(result).toBeGreaterThanOrEqual(-1.01);
-    expect(result).toBeLessThan(-0.99);
-    expect(result).toBeLessThanOrEqual(-0.99);
     expect(result).toBe(-1);
 
     result = simpleCalculator({ a: 1, b: 0, action: Action.Multiply });
-    expect(result).toBeLessThan(0.01);
-    expect(result).toBeLessThanOrEqual(0.01);
     expect(result).toBe(0);
-    expect(result).toBeGreaterThan(-0.01);
-    expect(result).toBeGreaterThanOrEqual(-0.01);
   });
 
   test('should divide two numbers', () => {
     let result = simpleCalculator({ a: 0.3, b: 0.2, action: Action.Divide });
-    expect(result).toBeLessThan(1.5);
-    expect(result).toBeLessThanOrEqual(1.5);
     expect(result).toBeCloseTo(1.5);
-    expect(result).toBeGreaterThan(1.49);
-    expect(result).toBeGreaterThanOrEqual(1.49);
 
     result = simpleCalculator({ a: 1, b: 0, action: Action.Divide });
     expect(result).toBe(Infinity);
@@ -64,18 +36,10 @@ describe('simpleCalculator tests', () => {
 
   test('should exponentiate two numbers', () => {
     let result = simpleCalculator({ a: 2, b: 3, action: Action.Exponentiate });
-    expect(result).toBeLessThan(8.1);
-    expect(result).toBeLessThanOrEqual(8.1);
     expect(result).toBe(8);
-    expect(result).toBeGreaterThan(7.9);
-    expect(result).toBeGreaterThanOrEqual(7.9);
 
     result = simpleCalculator({ a: 21, b: 0, action: Action.Exponentiate });
-    expect(result).toBeLessThan(1.01);
-    expect(result).toBeLessThanOrEqual(1.01);
     expect(result).toBe(1);
-    expect(result).toBeGreaterThan(0.99);
-    expect(result).toBeGreaterThanOrEqual(0.99);
   });
 
   test('should return null for invalid action', () => {

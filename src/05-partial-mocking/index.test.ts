c451850f7dcd4de7a223a9b029fcdb28a2a3da87
rahunak <euhene@linnovate.net>
2024-02-11 22:38:07 +0300

    #5: 05-partial-mocking

--- src/05-partial-mocking/index.test.ts
+++ src/05-partial-mocking/index.test.ts
@@ -1,8 +1,20 @@
 // Uncomment the code below and write your tests
-// import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
+import { mockOne, mockTwo, mockThree, unmockedFunction } from './index';
 
 jest.mock('./index', () => {
-  // const originalModule = jest.requireActual<typeof import('./index')>('./index');
+  const originalModule =
+    jest.requireActual<typeof import('./index')>('./index');
+
+  return {
+    // __esModule: true, // Use it when dealing with esModules.
+    // Return all original methods
+    ...originalModule,
+    // But rewrite some of them.
+    mockOne: jest.fn(),
+    mockTwo: jest.fn(),
+    mockThree: jest.fn(),
+    // For last test we have to check original method.
+  };
 });
 
 describe('partial mocking', () => {
@@ -11,10 +23,17 @@ describe('partial mocking', () => {
   });
 
   test('mockOne, mockTwo, mockThree should not log into console', () => {
-    // Write your test here
+    //Creates mock console.log function to spy on it.
+    const spyedConsoleLog = jest.spyOn(global.console, 'log');
+    mockOne();
+    mockTwo();
+    mockThree();
+    expect(spyedConsoleLog).not.toBeCalled();
   });
 
   test('unmockedFunction should log into console', () => {
-    // Write your test here
+    const spyedConsoleLog = jest.spyOn(global.console, 'log');
+    unmockedFunction();
+    expect(spyedConsoleLog).toHaveBeenCalled();
   });
 });
